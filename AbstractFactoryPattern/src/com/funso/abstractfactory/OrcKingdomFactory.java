73b465088d81d0184353f19f1a79ddc0ffec6f52
Musa Funso Oyedele <kehindet@gmail.com>
2025-09-03 17:52:40 +0100

    OrcKingFactoryClass added

---
+++ AbstractFactoryPattern/src/com/funso/abstractfactory/OrcKingdomFactory.java
@@ -0,0 +1,23 @@
+package com.funso.abstractfactory;
+
+public class OrcKingdomFactory implements KingdomFactory {
+
+	@Override
+	public Castle createCastle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public King createKing() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Army createArmy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
