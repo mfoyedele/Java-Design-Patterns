73a9a7dfb3d0b5f4795320a712ffd2f15a87d897
Musa Funso Oyedele <kehindet@gmail.com>
2025-09-03 17:06:47 +0100

    ElfCastle class implemented

---
+++ AbstractFactoryPattern/src/com/funso/abstractfactory/ElfCastle.java
@@ -0,0 +1,12 @@
+package com.funso.abstractfactory;
+
+public class ElfCastle implements Castle {
+	static final String DESCRIPTION = "This is the elven castle!";
+
+	@Override
+	public String getDecription() {
+		// TODO Auto-generated method stub
+		return DESCRIPTION;
+	}
+
+}
