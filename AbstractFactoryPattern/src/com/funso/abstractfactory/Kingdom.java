c710788afd12cedc6ea2949ebb3e50ec41f37a43
Musa Funso Oyedele <kehindet@gmail.com>
2025-09-03 18:02:09 +0100

    Update Kingdom class

--- AbstractFactoryPattern/src/com/funso/abstractfactory/Kingdom.java
+++ AbstractFactoryPattern/src/com/funso/abstractfactory/Kingdom.java
@@ -1,5 +1,31 @@
 package com.funso.abstractfactory;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Kingdom {
 
+  private King king;
+  private Castle castle;
+  private Army army;
+
+  /** The factory of kingdom factories. */
+  public static class FactoryMaker {
+
+    /** Enumeration for the different types of Kingdoms. */
+    public enum KingdomType {
+      ELF,
+      ORC
+    }
+
+    /** The factory method to create KingdomFactory concrete objects. */
+    public static KingdomFactory makeFactory(KingdomType type) {
+      return switch (type) {
+        case ELF -> new ElfKingdomFactory();
+        case ORC -> new OrcKingdomFactory();
+      };
+    }
+  }
 }
