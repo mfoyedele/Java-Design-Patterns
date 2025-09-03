cc7b1449487068af5f5e3acb5e2332a5429b4d5a
Musa Funso Oyedele <kehindet@gmail.com>
2025-09-03 17:37:20 +0100

    OrcKing class added

---
+++ AbstractFactoryPattern/src/com/funso/abstractfactory/OrcKing.java
@@ -0,0 +1,12 @@
+package com.funso.abstractfactory;
+
+public class OrcKing implements King {
+	static final String DESCRIPTION = "This is the orc King!";
+
+	@Override
+	public String getDecription() {
+		// TODO Auto-generated method stub
+		return DESCRIPTION;
+	}
+
+}
