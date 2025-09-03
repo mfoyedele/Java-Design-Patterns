cb905fb6413b69f5cffa8e091e69caccc578f97f
Musa Funso Oyedele <kehindet@gmail.com>
2025-09-03 17:09:08 +0100

    ElfKing class added

---
+++ AbstractFactoryPattern/src/com/funso/abstractfactory/ElfKing.java
@@ -0,0 +1,13 @@
+package com.funso.abstractfactory;
+
+public class ElfKing implements King {
+	
+	static final String DESCRIPTION = "This is the elven King!";
+
+	@Override
+	public String getDecription() {
+		// TODO Auto-generated method stub
+		return DESCRIPTION;
+	}
+
+}
