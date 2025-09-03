d87d2e0cb1d8116a3d5c70d7dd5883ff80e5965e
Musa Funso Oyedele <kehindet@gmail.com>
2025-09-03 17:11:26 +0100

    ElfArmy class added

---
+++ AbstractFactoryPattern/src/com/funso/abstractfactory/ElfArmy.java
@@ -0,0 +1,12 @@
+package com.funso.abstractfactory;
+
+public class ElfArmy implements Army {
+	static final String DESCRIPTION = "This is elven Army!";
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return DESCRIPTION;
+	}
+
+}
