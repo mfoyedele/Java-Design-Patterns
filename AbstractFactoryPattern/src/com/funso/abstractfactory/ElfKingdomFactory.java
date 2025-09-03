07572fd0e44eab0874ec97c3b9d4f05632d198a5
Musa Funso Oyedele <kehindet@gmail.com>
2025-09-03 17:46:48 +0100

    Update ElfKingdomFactory class

--- AbstractFactoryPattern/src/com/funso/abstractfactory/ElfKingdomFactory.java
+++ AbstractFactoryPattern/src/com/funso/abstractfactory/ElfKingdomFactory.java
@@ -1,5 +1,23 @@
 package com.funso.abstractfactory;
 
-public class ElfKingdomFactory {
+public class ElfKingdomFactory implements KingdomFactory {
+
+	@Override
+	public Castle createCastle() {
+		// TODO Auto-generated method stub
+		return new ElfCastle();
+	}
+
+	@Override
+	public King createKing() {
+		// TODO Auto-generated method stub
+		return new ElfKing();
+	}
+
+	@Override
+	public Army createArmy() {
+		// TODO Auto-generated method stub
+		return new ElfArmy();
+	}
 
 }
