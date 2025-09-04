fea9fedd8b2077c473bb8f8e1bbab89323f88d6b
Musa Funso Oyedele <kehindet@gmail.com>
2025-09-04 15:41:35 +0100

    AppTest class implemented

--- AbstractFactoryPattern/src/com/funso/abstractfactory/AppTest.java
+++ AbstractFactoryPattern/src/com/funso/abstractfactory/AppTest.java
@@ -1,5 +1,15 @@
 package com.funso.abstractfactory;
 
-public class AppTest {
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import org.junit.jupiter.api.Test;
+
+/** Check whether the execution of the main method in {@link App} throws an exception. */
+class AppTest {
+
+  @Test
+  void shouldExecuteApplicationWithoutException() {
+
+    assertDoesNotThrow(() -> App.main(new String[] {}));
+  }
 }
