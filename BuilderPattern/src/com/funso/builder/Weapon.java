f80ef7603e6b4a8690770c34516d6813acceae70
Musa Funso Oyedele <kehindet@gmail.com>
2025-09-06 13:11:35 +0100

    Weapon enum implemented

---
+++ BuilderPattern/src/com/funso/builder/Weapon.java
@@ -0,0 +1,15 @@
+package com.funso.builder;
+
+/** Weapon enumeration. */
+public enum Weapon {
+  DAGGER,
+  SWORD,
+  AXE,
+  WARHAMMER,
+  BOW;
+
+  @Override
+  public String toString() {
+    return name().toLowerCase();
+  }
+}
