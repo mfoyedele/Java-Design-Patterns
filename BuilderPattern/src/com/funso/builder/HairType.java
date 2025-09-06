bc34e107750d69b1bf3713bc61f877f48368577e
Musa Funso Oyedele <kehindet@gmail.com>
2025-09-06 13:08:04 +0100

    HairType enum implemented

---
+++ BuilderPattern/src/com/funso/builder/HairType.java
@@ -0,0 +1,20 @@
+package com.funso.builder;
+
+import lombok.AllArgsConstructor;
+
+/** HairType enumeration. */
+@AllArgsConstructor
+public enum HairType {
+  BALD("bald"),
+  SHORT("short"),
+  CURLY("curly"),
+  LONG_STRAIGHT("long straight"),
+  LONG_CURLY("long curly");
+
+  private final String title;
+
+  @Override
+  public String toString() {
+    return title;
+  }
+}
