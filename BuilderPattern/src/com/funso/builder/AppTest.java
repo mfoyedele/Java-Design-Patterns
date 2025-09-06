0507e3d69052e3a8efc979d76172c77947d33dcc
Musa Funso Oyedele <kehindet@gmail.com>
2025-09-06 13:29:49 +0100

    Update AppTest class

--- BuilderPattern/src/com/funso/builder/AppTest.java
+++ BuilderPattern/src/com/funso/builder/AppTest.java
@@ -1,46 +1,20 @@
 package com.funso.builder;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
 
-/** HeroTest */
-class HeroTest {
+/** Application test */
+class AppTest {
 
-  /** Test if we get the expected exception when trying to create a hero without a profession */
+  /**
+   * Issue: Add at least one assertion to this test case.
+   *
+   * <p>Solution: Inserted assertion to check whether the execution of the main method in {@link
+   * App} throws an exception.
+   */
   @Test
-  void testMissingProfession() {
-    assertThrows(IllegalArgumentException.class, () -> new Hero.Builder(null, "Sir without a job"));
-  }
-
-  /** Test if we get the expected exception when trying to create a hero without a name */
-  @Test
-  void testMissingName() {
-    assertThrows(IllegalArgumentException.class, () -> new Hero.Builder(Profession.THIEF, null));
-  }
-
-  /** Test if the hero build by the builder has the correct attributes, as requested */
-  @Test
-  void testBuildHero() {
-    final String heroName = "Sir Lancelot";
-
-    final var hero =
-        new Hero.Builder(Profession.WARRIOR, heroName)
-            .withArmor(Armor.CHAIN_MAIL)
-            .withWeapon(Weapon.SWORD)
-            .withHairType(HairType.LONG_CURLY)
-            .withHairColor(HairColor.BLOND)
-            .build();
-
-    assertNotNull(hero);
-    assertNotNull(hero.toString());
-    assertEquals(Profession.WARRIOR, hero.profession());
-    assertEquals(heroName, hero.name());
-    assertEquals(Armor.CHAIN_MAIL, hero.armor());
-    assertEquals(Weapon.SWORD, hero.weapon());
-    assertEquals(HairType.LONG_CURLY, hero.hairType());
-    assertEquals(HairColor.BLOND, hero.hairColor());
+  void shouldExecuteApplicationWithoutException() {
+    assertDoesNotThrow(() -> App.main(new String[] {}));
   }
 }
