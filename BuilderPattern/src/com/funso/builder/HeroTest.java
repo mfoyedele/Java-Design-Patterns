94172c9f52fe551378412bdd7ac28165528f6d85
Musa Funso Oyedele <kehindet@gmail.com>
2025-09-06 13:28:04 +0100

    Update HeroTest class

--- BuilderPattern/src/com/funso/builder/HeroTest.java
+++ BuilderPattern/src/com/funso/builder/HeroTest.java
@@ -1,5 +1,46 @@
 package com.funso.builder;
 
-public class HeroTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.Test;
+
+/** HeroTest */
+class HeroTest {
+
+  /** Test if we get the expected exception when trying to create a hero without a profession */
+  @Test
+  void testMissingProfession() {
+    assertThrows(IllegalArgumentException.class, () -> new Hero.Builder(null, "Sir without a job"));
+  }
+
+  /** Test if we get the expected exception when trying to create a hero without a name */
+  @Test
+  void testMissingName() {
+    assertThrows(IllegalArgumentException.class, () -> new Hero.Builder(Profession.THIEF, null));
+  }
+
+  /** Test if the hero build by the builder has the correct attributes, as requested */
+  @Test
+  void testBuildHero() {
+    final String heroName = "Sir Lancelot";
+
+    final var hero =
+        new Hero.Builder(Profession.WARRIOR, heroName)
+            .withArmor(Armor.CHAIN_MAIL)
+            .withWeapon(Weapon.SWORD)
+            .withHairType(HairType.LONG_CURLY)
+            .withHairColor(HairColor.BLOND)
+            .build();
+
+    assertNotNull(hero);
+    assertNotNull(hero.toString());
+    assertEquals(Profession.WARRIOR, hero.profession());
+    assertEquals(heroName, hero.name());
+    assertEquals(Armor.CHAIN_MAIL, hero.armor());
+    assertEquals(Weapon.SWORD, hero.weapon());
+    assertEquals(HairType.LONG_CURLY, hero.hairType());
+    assertEquals(HairColor.BLOND, hero.hairColor());
+  }
 }
